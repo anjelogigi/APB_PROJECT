c11dbd55bd03f384b4ce5a1b2f4702d04deeccfc
anjelogigi <anjelogigi@gmail.com>
2026-07-20 10:13:16 +0530

    Update apb_generator.sv

--- SRC/testbench/apb_generator.sv
+++ SRC/testbench/apb_generator.sv
@@ -1,7 +1,8 @@
 `include "defines.svh"
+
 class apb_generator;
+
     apb_transaction blueprint;
-    apb_transaction trans;
     mailbox #(apb_transaction) mbx_gd;
 
     function new(mailbox #(apb_transaction) mbx_gd);
@@ -10,12 +11,36 @@ class apb_generator;
     endfunction
 
     task start();
-        for (int i = 0; i < `NUM_TRANSACTIONS; i++)
-        begin
-            trans = blueprint.copy();
-            assert(trans.randomize())
-            mbx_gd.put(trans.copy());
+
+        for (int i = 0; i < `NUM_TRANSACTIONS; i++) begin
+
+            // Random Test
+            assert(blueprint.randomize());
+
+            // Write Test
+            //assert(blueprint.randomize() with { PWRITE == 1; });
+
+            // Read Test
+            //assert(blueprint.randomize() with { PWRITE == 0; });
+
+            // Invalid Address Test
+            //assert(blueprint.randomize() with { PADDR >= `DATA_DEPTH; });
+
+            // No Strobe Test
+            //assert(blueprint.randomize() with {
+            //    PWRITE == 1;
+            //    PSTRB  == 4'b0000;
+            //});
+
+            // Back-to-Back Test
+            //assert(blueprint.randomize() with {
+            //    PADDR inside {[0:`DATA_DEPTH-1]};
+            //});
+
+            mbx_gd.put(blueprint.copy());
+
         end
+
     endtask
 
 endclass
